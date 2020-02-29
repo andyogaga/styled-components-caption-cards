266750f7cce9783a0b9f5a5d17f950e37ef1acee
andy <andrewo@shleeq.com>
2020-02-29 04:02:10 +0100

    added navbar and home jsx files

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar, LogoContainer, MenuContainer } from '.'
+
+const NavBar = props => {
+  return (
+    <Navbar >
+      <LogoContainer />
+      <MenuContainer />
+    </Navbar>
+  )
+}
+
+export default NavBar
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {Container} from '../../components'
+import NavBar from '../../components/NavBar'
+
+const Home = () => {
+  return (
+    <Container>
+      <NavBar />
+      <div >
+         <p>I am here</p>
+      </div>
+     
+    </Container>
+  )
+}
+
+export default Home
