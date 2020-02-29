a6d159d1cab8350fdba2fd85fb703c7e3853563c
andy <andrewo@shleeq.com>
2020-02-29 14:30:10 +0100

    fixing the alignment of the tag array

--- src/components/index.js
+++ src/components/index.js
@@ -54,6 +54,7 @@ export const SearchSection = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: center;
+  flex-direction: column;
 
   @media screen and ${breakpoints.sm} {
     justify-content: center;
